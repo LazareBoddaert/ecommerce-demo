eccec0e5fd36b9dffe5a9a525e00f43b4f62f46b
Lazare Boddaert <lazare.boddaert@gmail.com>
2023-03-08 15:48:57 +0100

    add Sanity client info + heroBanner component

--- e-commerce_demo_frontend/src/pages/_app.js
+++ e-commerce_demo_frontend/src/pages/_app.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import '@component/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
--- e-commerce_demo_frontend/src/pages/index.js
+++ e-commerce_demo_frontend/src/pages/index.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-const index = () => {
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const index = ({ products, bannerData }) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers</p>
       </div>
       <div className="products-container">
-        {['Prod 1', 'Prod 2', 'Prod 3'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]';
+  const products = await client.fetch(productQuery);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default index
